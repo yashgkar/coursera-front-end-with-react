7d8f001ce0a1355d56256ad1aa32a0671d4a3114
Yash Garudkar <yashgarudkar@gmail.com>
2020-07-01 22:47:46 +0530

    Functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,86 +1,77 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle, CardImgOverlay, Button } from "reactstrap";
+import React from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, CardImgOverlay } from "reactstrap";
 import { X } from "react-bootstrap-icons";
 
-class DishDetail extends Component {
-  componentDidMount() {
-    console.log('conponentdid mount')
-  }
 
-  componentDidUpdate() {
-    console.log('conponentdidupdate')
-  }
 
-  renderDish(dish) {
-    console.log('render')
-    if (dish != null) {
-      return (
-        <div>
-          <div className='row'>
-            <div className='col-12 col-md-5 m-1'>
-              <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                  <CardTitle>
-                    <X size={25} onClick={() => this.props.onRemove()}/>
-                  </CardTitle>
-                </CardImgOverlay>
-
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
-              </Card>
-            </div>
-            <div className='col-12 col-md-5 m-1'>
-              <h4>Comments</h4>
-              {this.renderComments(dish.comments)}
-            </div>
-          </div>
-        </div>
-
-      );
-    } else {
-      return (
-        <div></div>
-      );
-    }
-  }
-
-  renderComments(comments) {
-    if (comments != null) {
-      const com = comments.map((comment) => {
-        return (
-          <div key={comment.id}>
-            <CardText>
-              <li>{comment.comment}</li>
-              <li>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-IN', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} `}</li>
-            </CardText>
-          </div>
-        )
-      })
-      return (
-        <ul className="list-unstyled">
-          {com}
-        </ul>
-      )
-
-    } else {
-      return (<div></div>)
-    }
-  }
-
-  render() {
-
+function RenderDish({ dish, onRemove }) {
+  if (dish != null) {
     return (
-      <div className="container">
-        <div className="row">
-          {this.renderDish(this.props.dish)}
+      <div>
+        <div className='row'>
+          <div className='col-12 col-md-5 m-1'>
+            <Card>
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+                <CardTitle>
+                  <X size={25} onClick={onRemove} />
+                </CardTitle>
+              </CardImgOverlay>
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='col-12 col-md-5 m-1'>
+            <h4>Comments</h4>
+            <RenderComments comments={dish.comments} />
+          </div>
         </div>
       </div>
+
+    );
+  } else {
+    return (
+      <div></div>
     );
   }
-
 }
+
+function RenderComments({ comments }) {
+  if (comments != null) {
+    const com = comments.map((comment) => {
+      return (
+        <div key={comment.id}>
+          <CardText>
+            <li>{comment.comment}</li>
+            <li>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-IN', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} `}</li>
+          </CardText>
+        </div>
+      )
+    })
+    return (
+      <ul className="list-unstyled">
+        {com}
+      </ul>
+    )
+
+  } else {
+    return (<div></div>)
+  }
+}
+
+const DishDetail = (props) => {
+
+  return (
+    <div className="container">
+      <div className="row">
+        <RenderDish dish={props.dish} onRemove={props.onRemove} />
+      </div>
+    </div>
+  );
+}
+
+
 
 export default DishDetail;
