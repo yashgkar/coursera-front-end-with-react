ae10e115e65af1261aeac29a3a67633464f74198
Yash Garudkar <yashgarudkar@gmail.com>
2020-07-18 02:21:14 +0530

    Added redux form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,21 +1,30 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { Dishes } from "./dishes";
-import { Comments } from "./comments";
-import { Promotions } from "./promotions";
-import { Leaders } from "./leaders";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createForms } from 'react-redux-form'
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
+
 
 export const ConfigureStore = () => {
- const store = createStore(
-  combineReducers({
-   dishes: Dishes,
-   comments: Comments,
-   promotions: Promotions,
-   leaders: Leaders
-  }),
-  applyMiddleware(thunk, logger)
- );
-
- return store;
+    const store = createStore(
+        combineReducers(
+            {
+                dishes: Dishes,
+                comments: Comments,
+                promotions: Promotions,
+                leaders: Leaders,
+                ...createForms(
+                    {
+                        feedback: InitialFeedback
+                    }
+                )
+            }
+        ),
+        applyMiddleware(thunk, logger)
+    )
+    return store;
 }
